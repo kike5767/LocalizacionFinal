466e46fd2fb10fe1265979ed3f9b891c300f7f9b
Mateo Vallejo Alvarez <Teo_Alvarez21@hotmail.com>
2024-11-10 20:45:44 -0500

    Capa de servicios (asp_servicios: controladores, startup, token, prueba unitaria configuracion)

---
+++ asp_servicios/Startup.cs
@@ -0,0 +1,63 @@
+﻿using asp_servicios.Controllers;
+using lib_aplicaciones.Implementaciones;
+using lib_aplicaciones.Interfaces;
+using lib_repositorios;
+using lib_repositorios.Implementaciones;
+using lib_repositorios.Interfaces;
+using Microsoft.AspNetCore.Server.Kestrel.Core;
+
+namespace asp_servicios
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public static IConfiguration? Configuration { set; get; }
+
+        public void ConfigureServices(WebApplicationBuilder builder, IServiceCollection services)
+        {
+            services.Configure<KestrelServerOptions>(x => { x.AllowSynchronousIO = true; });
+            services.Configure<IISServerOptions>(x => { x.AllowSynchronousIO = true; });
+            services.AddControllers();
+            services.AddEndpointsApiExplorer();
+            services.AddSwaggerGen();
+            services.AddScoped<Conexion, Conexion>();
+            // Repositorios
+            services.AddScoped<IDetallesImagenesRepositorio, DetallesImagenesRepositorio>();
+            services.AddScoped<IImagenesRepositorio, ImagenesRepositorio>();
+            services.AddScoped<ILocalidadesRepositorio, LocalidadesRepositorio>();
+            services.AddScoped<ILocalizacionesRepositorio, LocalizacionesRepositorio>();
+            services.AddScoped<IPersonasRepositorio, PersonasRepositorio>();
+            services.AddScoped<IUbicacionesRepositorio, UbicacionesRepositorio>();
+            // Aplicaciones
+            services.AddScoped<IDetallesImagenesAplicacion, DetallesImagenesAplicacion>();
+            services.AddScoped<IImagenesAplicacion, ImagenesAplicacion>();
+            services.AddScoped<ILocalidadesAplicacion,LocalidadesAplicacion>();
+            services.AddScoped<ILocalizacionesAplicacion, LocalizacionesAplicacion>();
+            services.AddScoped<IPersonasAplicacion, PersonasAplicacion>();
+            services.AddScoped<IUbicacionesAplicacion, UbicacionesAplicacion>();
+            // Controladores
+            services.AddScoped<TokenController, TokenController>();
+
+            services.AddCors(o => o.AddDefaultPolicy(b => b.AllowAnyOrigin()));
+        }
+
+        public void Configure(WebApplication app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+            app.UseHttpsRedirection();
+            app.UseAuthorization();
+            app.MapControllers();
+            app.Run();
+            app.UseRouting();
+            app.UseCors();
+        }
+    }
+}
